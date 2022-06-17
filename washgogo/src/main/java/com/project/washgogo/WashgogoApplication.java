5d5cb1106a6d8591304eb07bd991760183f25ca8
Wjaehyun <wjaehyun97@gmail.com>
2022-06-17 21:47:18 +0900

    gitignore

---
+++ washgogo/src/main/java/com/project/washgogo/WashgogoApplication.java
@@ -0,0 +1,13 @@
+package com.project.washgogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashgogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WashgogoApplication.class, args);
+    }
+
+}
