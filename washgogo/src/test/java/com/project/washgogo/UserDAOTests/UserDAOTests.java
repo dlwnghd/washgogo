16c90556afa79abfb4d7bab39bcbebdb60439c93
Wjaehyun <wjaehyun97@gmail.com>
2022-06-20 20:55:25 +0900

    문자 오류 수정

--- washgogo/src/test/java/com/project/washgogo/UserDAOTests/UserDAOTests.java
+++ washgogo/src/test/java/com/project/washgogo/UserDAOTests/UserDAOTests.java
@@ -1,37 +1,37 @@
-package com.project.washgogo.UserDAOTests;
-
-import com.project.washgogo.domain.dao.UserDAO;
-import com.project.washgogo.domain.vo.UserVO;
-import com.project.washgogo.mapper.UserMapper;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@Slf4j
-public class UserDAOTests {
-    @Autowired
-    private UserDAO userDAO;
-
-    @Test
-    public void getListTest(){
-        userDAO.getList().stream().map(UserVO::toString).forEach(log::info);
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//package com.project.washgogo.UserDAOTests;
+//
+//import com.project.washgogo.domain.dao.UserDAO;
+//import com.project.washgogo.domain.vo.UserVO;
+//import com.project.washgogo.mapper.UserMapper;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//@Slf4j
+//public class UserDAOTests {
+//    @Autowired
+//    private UserDAO userDAO;
+//
+//    @Test
+//    public void getListTest(){
+//        userDAO.getList().stream().map(UserVO::toString).forEach(log::info);
+//    }
+//
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
