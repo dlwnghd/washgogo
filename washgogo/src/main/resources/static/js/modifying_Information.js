1b9d5baf6960bbe6becbab8c11c09497c9a3b516
Lee Juhong <alion0513@naver.com>
2022-06-24 17:17:55 +0900

    이주홍/ HTML/CSS/JS 수정

--- washgogo/src/main/resources/static/js/modifying_Information.js
+++ washgogo/src/main/resources/static/js/modifying_Information.js
@@ -7,7 +7,6 @@ const $password = $("#PASSWORD");
 const $phonenumber = $("#PHONENUMBER");
 const $address = $("#ADDRESS");
 const $addressdetail = $("#ADDRESSDETAIL");
-const $payment = $("#PAYMENT");
 
 const $inputInfo = $("#modifyInfo");
 const $pw = $("#pw");
@@ -16,7 +15,6 @@ const $result = $("#result");
 //  모달창에 있는 확인창이 눌리면
 const changeuserBtn = document.querySelector(".change-user");
 changeuserBtn.addEventListener('click', e => {
-    console.log(changeInfo.innerText);
     let type = changeInfo.innerText;
 
     switch (type) {
@@ -31,7 +29,8 @@ changeuserBtn.addEventListener('click', e => {
         case '비밀번호 수정':
             if (document.getElementById("Pw").value != document.getElementById("PwCheck").value) {
                 alert("비밀번호가 일치하지 않습니다!");
-                newPwCheck.innerHTML = "<input type=\"password\">";
+                newPw.innerHTML = "비밀번호 확인<div class=\"password-wrapper npw\"><input id=\"Pw\" type=\"password\"><button class=\"pw-show\"></button></div>";
+                newPwCheck.innerHTML = "새 비밀번호 확인<div class=\"password-wrapper npw\"><input id=\"PwCheck\" type=\"password\"><button class=\"pw-show\"></button></div>";
                 break;
             }
             // 비밀번호는 db에 전달만 하고 view에서는 보이지 않게 하도록 하자
@@ -48,16 +47,13 @@ changeuserBtn.addEventListener('click', e => {
         case '공동현관 출입방법 수정':
             $addressdetail.text(document.getElementById("result").value); // ⭐기능 구현되는 코드
             break;
-        case '결제정보 수정':
-            $payment.text(document.getElementById("result").value); // ⭐기능 구현되는 코드
-            break;
     }
     close();
 })
 
 function close() {
     modal.style.display = "none";
-    modal_window.style.height = '250px';
+    modal_window.style.height = '300px';
     // 비밀번호 지우기❌
     newPw.style.display = "none";
     newPwCheck.style.display = "none";
@@ -66,6 +62,8 @@ function close() {
     choose2.style.display = "none";
     // 수정칸 지우기❌
     modifyInfo.style.display = 'none';
+    document.getElementById('Pw').value = "";
+    document.getElementById('PwCheck').value = "";
 }
 // 취소 버튼으로 닫기
 const nochangeBtn = document.querySelector(".no-change-user");
@@ -104,90 +102,88 @@ const nameBtn = account.querySelector(".name")
 nameBtn.addEventListener("click", e => {
     modal.style.display = "grid";
     // 수정할 데이터명
-    changeInfo.innerHTML = '<h2 style="text-align: center;">이름 수정</h2>';
-    modifyInfo.innerHTML = "이름<input id=\"result\" type=\"text\">";
+    changeInfo.innerHTML = '<h2>이름 수정</h2>';
+    modifyInfo.innerHTML = "이름<input id=\"result\" type=\"text\" placeholder=\"이름을 입력해주세요\">";
     // 수정공간 나타내기
     modifyInfo.style.display = 'block';
-    choose1.style.display = 'block';
+    choose1.style.display = 'contents';
 })
 // 이메일 수정 모달 띄우기
 const emailBtn = account.querySelector(".email")
 emailBtn.addEventListener("click", e => {
     modal.style.display = "grid";
     // 수정할 데이터명
-    changeInfo.innerHTML = '<h2 style="text-align: center;">이메일 수정</h2>';
-    modifyInfo.innerHTML = "이메일<input id=\"result\" type=\"text\">";
+    changeInfo.innerHTML = '<h2>이메일 수정</h2>';
+    modifyInfo.innerHTML = "이메일<input id=\"result\" type=\"text\" placeholder=\"이메일을 입력해주세요\">";
     // 수정공간 나타내기
     modifyInfo.style.display = 'block';
-    choose1.style.display = 'block';
+    choose1.style.display = 'contents';
 })
 // 비밀번호 수정 모달 띄우기
 const passwordBtn = account.querySelector(".password")
 passwordBtn.addEventListener("click", e => {
     modal.style.display = "grid"
-    changeInfo.innerHTML = '<h2 style="text-align: center;">비밀번호 수정</h2>';
-    modal_window.style.height = '300px';
-    // 수정할 데이터명
-    newPw.innerHTML = "새 비밀번호<input id=\"Pw\" type=\"password\">";
-    newPwCheck.innerHTML = "새 비밀번호 확인<input id=\"PwCheck\" type=\"password\">";
+    changeInfo.innerHTML = '<h2>비밀번호 수정</h2>';
+    modal_window.style.height = '360px';
     // 비밀번호 나타내기
     newPw.style.display = "block";
     newPwCheck.style.display = "block";
     // 수정공간 나타내기
-    choose1.style.display = 'block';
+    choose1.style.display = 'contents';
 })
 // 휴대전화번호 수정 모달 띄우기
 const phoneNumberBtn = account.querySelector(".phoneNumber")
 phoneNumberBtn.addEventListener("click", e => {
     modal.style.display = "grid";
     // 수정할 데이터명
-    changeInfo.innerHTML = '<h2 style="text-align: center;">휴대전화 번호 수정</h2>';
+    changeInfo.innerHTML = '<h2>휴대전화 번호 수정</h2>';
     modifyInfo.innerHTML = "휴대전화<input id=\"result\" type=\"text\">";;
     // 수정공간 나타내기
     modifyInfo.style.display = 'block';
-    choose1.style.display = 'block';
+    choose1.style.display = 'contents';
 })
 // 배송지 수정 모달 띄우기
 const addressBtn = account.querySelector(".address")
 addressBtn.addEventListener("click", e => {
     modal.style.display = "grid";
     // 수정할 데이터명
-    changeInfo.innerHTML = '<h2 style="text-align: center;">배송지 수정</h2>';
+    changeInfo.innerHTML = '<h2>배송지 수정</h2>';
     modifyInfo.innerHTML = "배송지<input id=\"result\" type=\"text\">";;
     // 수정공간 나타내기
     modifyInfo.style.display = 'block';
-    choose1.style.display = 'block';
+    choose1.style.display = 'contents';
 })
 //  공동현관 출입방법 모달 띄우기
 const addressDetailBtn = account.querySelector(".addressDetail")
 addressDetailBtn.addEventListener("click", e => {
     modal.style.display = "grid";
     // 수정할 데이터명
-    changeInfo.innerHTML = '<h2 style="text-align: center;">공동현관 출입방법 수정</h2>';
+    changeInfo.innerHTML = '<h2>공동현관 출입방법 수정</h2>';
     modifyInfo.innerHTML = "공동현관 출입방법<input id=\"result\" type=\"text\">";;
     // 수정공간 나타내기
     modifyInfo.style.display = 'block';
-    choose1.style.display = 'block';
-})
-// 결제정보 수정 모달 띄우기
-const paymentBtn = account.querySelector(".payment")
-paymentBtn.addEventListener("click", e => {
-    modal.style.display = "grid";
-    // 수정할 데이터명
-    changeInfo.innerHTML = '<h2 style="text-align: center;">결제정보 수정</h2>';
-    modifyInfo.innerHTML = "결제정보<input id=\"result\" type=\"text\">";;
-    // 수정공간 나타내기
-    modifyInfo.style.display = 'block';
-    choose1.style.display = 'block';
+    choose1.style.display = 'contents';
 })
 // 계정탈퇴 모달 띄우기
 const accountWithdrawalBtn = withdrawal.querySelector(".accountWithdrawal")
 accountWithdrawalBtn.addEventListener("click", e => {
     modal.style.display = "grid";
-    changeInfo.innerHTML = '<h2 style="text-align: center;">정말 WASHGOGO를<br>떠나실 건가요?</h2>';
-    modal_window.style.height = '300px';
+    changeInfo.innerHTML = '<h2>정말 WASHGOGO를<br>떠나실 건가요?</h2>';
+    modal_window.style.height = '350px';
     // 계정 탈퇴 선택창 나타내기
     withdrawals.style.display = "block";
-    choose2.style.display = "block";
+    choose2.style.display = "contents";
     choose1.style.display = "none";
 })
+
+/* 비밀번호 보이기, 감추기 */
+$(".pw-show").on("click",function(){
+    $("input").toggleClass("active");
+    if($("input").hasClass("active")){
+        $(".pw-show").prev("input").attr("type","text");
+        $(".pw-show").toggleClass("pw-hide");
+    }else{
+        $(".pw-show").prev("input").attr("type","password");
+        $(".pw-show").removeClass("pw-hide");
+    }
+});
