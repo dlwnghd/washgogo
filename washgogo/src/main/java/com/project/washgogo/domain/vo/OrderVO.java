0ce4d5b742051288ac1d1fb0a664640b365cade0
polodumbo <polosh1208@gmail.com>
2022-07-04 16:21:31 +0900

    VO 객체 변수 주석

--- washgogo/src/main/java/com/project/washgogo/domain/vo/OrderVO.java
+++ washgogo/src/main/java/com/project/washgogo/domain/vo/OrderVO.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class OrderVO {
-	private Long orderNumber;
-	private Long userNumber;
-	private String orderDate;
-	private String orderProgress;
-	private String orderAlert;
-	private String orderRequestMessage;
-	private Long orderTotalPrice;
+	private Long orderNumber;           // 주문번호(SEQUENCE, PK)
+	private Long userNumber;            // 사용자번호(USER FK)
+	private String orderDate;           // 주문일자(DEFAULT SYSDATE)
+	private String orderProgress;       // 진행도(DEFAULT '세탁 중')
+	private String orderAlert;          // 수거 시 알림 시간
+	private String orderRequestMessage; // 세탁 요청 사항
+	private Long orderTotalPrice;       // 총액
 }
--- washgogo/src/main/java/com/project/washgogo/domain/vo/UserVO.java
+++ washgogo/src/main/java/com/project/washgogo/domain/vo/UserVO.java
@@ -6,17 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class UserVO {
-	private Long userNumber;
-	private String userName;
-	private String userPw;
-	private String userEmail;
-	private String userPhonenum;
-	private String userAddress;
-	private String userAddressDetail;
-	private String userEntranceType;
-	private String userEntrancePw;
-	private String userEntranceMessage;
-	private String userServiceType;
-	private String userLaunderetteType;
-	private String userPoint;
+	private Long userNumber;            // 사용자 번호(SEQUENCE, PK)
+	private String userEmail;           // 이메일(UNIQUE)
+	private String userPw;              // 비밀번호
+	private String userName;            // 사용자 이름
+	private String userPhonenum;        // 전화번호
+	private String userAddress;         // 주소
+	private String userAddressDetail;   // 상세주소
+	private String userEntranceType;    // 출입문 종류
+	private String userEntrancePw;      // 공동현관 비밀번호
+	private String userEntranceMessage; // 공동현관 메시지
+	private String userServiceType;     // 서비스 타입
+	private String userLaunderetteType; // 런드렛 타입
+	private String userPoint;           // 포인트
 }
