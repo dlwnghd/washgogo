9b5827af81d8f0d3f87a6a8a14f3661cdb1e0bdd
Lee Juhong <alion0513@naver.com>
2022-07-06 16:12:36 +0900

    이주홍 / 서비스 변경 시 값이 영어로 뜨는 것에서 한글로 뜨도록 변경 및 DB에 저장할 때도 한글로 저장하도록 수정

--- washgogo/src/main/resources/static/js/service_change.js
+++ washgogo/src/main/resources/static/js/service_change.js
@@ -3,7 +3,7 @@ const changeOnceText = document.getElementById('chooseOnceText');
 const changeSeveralButton = $("#severalButton");
 const changeOnceButton = $("#onceButton");
 var userServiceTypeValue = document.getElementById("userServiceType");
-var userLaunderetteValue = document.getElementById("userServiceType");
+var userLaunderetteValue = document.getElementById("userLaunderetteType");
 
 // 여러 번 이용 버튼을 눌렀을 경우
 changeSeveralButton .click(function () {
@@ -16,7 +16,7 @@ changeSeveralButton .click(function () {
     // ⭐후에 시간 남으면 h3를 없애고 div에 text(ex.변경하기)를 넣고 아래와 같이 변경하기
     changeSeveralText.innerHTML = '서비스 이용 중';
     changeOnceText.innerHTML = '변경하기';
-    userServiceTypeValue.value = "Several";
+    userServiceTypeValue.value = "여러 번 이용";
     userLaunderetteValue = "Regular"
 });
 
@@ -30,7 +30,7 @@ changeOnceButton.click(function () {
     changeSeveralButton.prop("type", "submit");
     changeSeveralText.innerHTML = '변경하기';
     changeOnceText.innerHTML = '서비스 이용 중';
-    userServiceTypeValue.value = "Once";
+    userServiceTypeValue.value = "한 번 이용";
     userLaunderetteValue = "선택안함"
 });
 
